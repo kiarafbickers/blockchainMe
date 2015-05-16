a862cb78c2adab84ea885a9473814eac33f7df82
Kiara Robles <kiara.robles@gmail.com>
2015-05-16 09:07:12 -0700

    update
    update

--- test/file-to-address/main.js
+++ test/file-to-address/main.js
@@ -2,6 +2,17 @@
 
 var BROWSER_UNSUPPORTED_MESSAGE = "Error: Browser not supported\nReason: We need a cryptographically secure PRNG to be implemented (i.e. the window.crypto method)\nSolution: Use Chrome >= 11, Safari >= 3.1 or Firefox >= 21";
 
+function getHashFileFromHtml() {
+    return $('#fileToHash2').val();
+}
+
+function updateHtmlWithHash(hash) {
+    $('#value').val(value);
+}
+
+function getHashFromHtml() {
+    return $('#fileToAddress').val();
+}
 
 function generateBitconAddress() {
     var hash = sha256_digest(getFileFromHtml);
@@ -9,13 +20,6 @@ function generateBitconAddress() {
     var hash = "00" + hash;        
 } 
 
-function getFileFromHtml() {
-    return $('#fileToAddress').val();
-}
-
-function updateHtmlWithHash(hash) {
-    $('#hash').val(hash);
-}
 
 function handleError(error) {
     console.log(error);
